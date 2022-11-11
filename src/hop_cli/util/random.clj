57b214b564036cb164a915f6b735324a006e8b16
Lucas Sousa <lucas.sousa@magnet.coop>
2022-11-11 17:28:53 +0100

    Add util.random ns to generate random strings

---
+++ src/hop_cli/util/random.clj
@@ -0,0 +1,18 @@
+(ns hop-cli.util.random
+  (:import [java.security SecureRandom]))
+
+(def ^:private rng (SecureRandom.))
+
+(def ^:private chs
+  (map char (concat (range 48 58) ; 0-9
+                    (range 65 91) ; A-Z
+                    (range 97 123) ; a-z
+                    [\~ \! \@ \# \$ \% \^ \& \* \( \) \- \+ \=])))
+
+(defn- generate-random-character
+  []
+  (nth chs (.nextInt rng (count chs))))
+
+(defn generate-random-string
+  [length]
+  (apply str (take length (repeatedly generate-random-character))))
