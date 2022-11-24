f1885a70b0aeb7a836f6f612c7bc834bea585956
Lucas Sousa <lucas.sousa@magnet.coop>
2022-11-24 17:24:07 +0100

    Impl mechanism to allow printing personalized error messsages
    * It's a very simple change but it allows to print error messages
    defined in the spec of each CLI option given a cause. Otherwise it
    prints the generic error message generate by babashka.cli.

--- src/hop_cli/util/error.clj
+++ src/hop_cli/util/error.clj
@@ -1,9 +1,13 @@
 (ns hop-cli.util.error
   (:require [babashka.cli :as cli]))
 
+(defn get-error-msg
+  [{:keys [spec cause msg option]}]
+  (or (get-in spec [option :error-msgs cause]) msg))
+
 (defn generic-error-handler
-  [{:keys [msg spec]}]
-  (println "An error has occurred:" msg)
+  [{:keys [spec] :as error}]
+  (println "An error has occurred:" (get-error-msg error))
   (println "Usage:")
   (println (cli/format-opts {:spec spec}))
   (System/exit 1))
