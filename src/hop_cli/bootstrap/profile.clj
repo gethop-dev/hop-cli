2b2b5d33a0edbfe0d91a73f09018b4e5ca4e5c80
Lucas Sousa <lucas.sousa@magnet.coop>
2022-12-15 14:58:51 +0100

    Ignore temp-dir unresolved-symbol warning in copy-files!

--- src/hop_cli/bootstrap/profile.clj
+++ src/hop_cli/bootstrap/profile.clj
@@ -32,6 +32,7 @@
         (fs/copy-tree src-path dst-path {:replace-existing true})
         (fs/copy src-path dst-path {:replace-existing true})))))
 
+#_{:clj-kondo/ignore [:unresolved-symbol]}
 (defn- copy-files!
   [settings]
   (if-let [jar-file-path (get-jar-file-path)]
