1e908d91fd42ec765ecdb5c67a7afbe351c56f93
Bingen Galartza Iparragirre <bingen.galartza@magnet.coop>
2022-12-20 15:22:20 +0100

    Add dc.sh utility script to core profile
    [Re #2]
    A convenience script for running docker-compose with the right
    docker-compose files selected.

---
+++ resources/bootstrap/profiles/core/dc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+# Tell docker-compose what compose files to use to run the different commands below
+# Order of the files IS IMPORTANT, as later files overwrite values from previous ones.
+# See https://docs.docker.com/compose/reference/envvars/#compose_file
+export COMPOSE_FILE="{{project.docker-compose.to-develop}}"
+
+docker-compose "$@"
