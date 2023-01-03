72bfb262b588bb8f94fc9aabafd675aec5e88f5c
Lucas Sousa <lucas.sousa@magnet.coop>
2023-01-03 08:44:04 +0100

    Improve docker-env-vars.sh to work with docker-compose v1.x and v2.x
    * Apart from gaining compatibility with v2.x (before it would only
    work with v1.x), the approach to check the environment variables
    changed to be more strict.

--- resources/bootstrap/profiles/core/docker/docker-env-vars.sh
+++ resources/bootstrap/profiles/core/docker/docker-env-vars.sh
@@ -2,10 +2,40 @@
 
 set -eu -o pipefail
 
-vars="$(docker-compose config)"
-if [[ $(echo "${vars}" | grep -Fc ': null') -gt 0 ]]; then
+function get_missing_env_vars() {
+    if docker-compose config --no-normalize >/dev/null 2>/dev/null; then
+        # docker-compose v2.x
+        COMPAT_ARG='--no-normalize'
+    else
+        # docker-compose v1.x
+        COMPAT_ARG=''
+    fi
+
+    #shellcheck disable=SC2086
+    docker-compose config ${COMPAT_ARG} |
+        docker run --rm -i mikefarah/yq '.services[].environment?
+            |=
+            (
+                with(select(. == null);
+                    . = []
+                )
+                |
+                to_entries
+                |
+                map(
+                    select((.value == null))
+                    |
+                    .key  "="  ">>HOP_ENV_VAR_MISSING_VALUE<<"
+                )
+            )' |
+        awk -F'=' '/>>HOP_ENV_VAR_MISSING_VALUE<</ {print $1}'
+}
+
+missing_vars=$(get_missing_env_vars)
+
+if [[ -n "${missing_vars}" ]]; then
     echo "Error: Some environment variables are missing:"
-    echo "${vars}" | awk -F':' '/: null/ {print $1}'
+    echo "${missing_vars}"
     exit 1
 else
     exit 0
