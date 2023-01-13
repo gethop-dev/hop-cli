74b800e1bc852c243913e6b0f6ee2b0a765db1af
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2023-01-13 15:56:18 +0100

    Make SQL instructions post-install step a bit clearer.
    Right now the SQL script instructions follow the sentence about
    executing the script itself, whithout any separation. And it's a bit
    unclear where the SQL commands to execute begin and end. Add a bit of
    whitespace before and after, and add some SQL comments saying where it
    starts and ends.

--- resources/bootstrap/profiles/persistence/sql/postgres/init-scripts/to-deploy/test/01_create_schemas_and_roles.sql
+++ resources/bootstrap/profiles/persistence/sql/postgres/init-scripts/to-deploy/test/01_create_schemas_and_roles.sql
@@ -1,9 +1,14 @@
+-- SQL script begins here
+-- 
 -- SQL for creating the schemas and roles used by the project.
 -- This SQL script must be executed manually using the admin credentials:
--- DB host: ${DB_HOST}
--- DB name: ${DB_NAME}
--- User: ${DB_ADMIN_USER}
--- Password: ${DB_ADMIN_PASSWORD}
+-- 
+--     DB host: ${DB_HOST}
+--     DB port: ${DB_PORT}
+--     DB name: ${DB_NAME}
+--     User: ${DB_ADMIN_USER}
+--     Password: ${DB_ADMIN_PASSWORD}
+-- 
 
 {{>bootstrap/profiles/persistence/sql/postgres/init-scripts/create_schemas_and_roles.app.sql}}
 
@@ -14,3 +19,5 @@
 {{#project.profiles.persistence-sql.deployment.to-deploy.rds.environment.test.database.other-users.grafana.enabled}}
 {{>bootstrap/profiles/persistence/sql/postgres/init-scripts/create_schemas_and_roles.grafana.sql}}
 {{/project.profiles.persistence-sql.deployment.to-deploy.rds.environment.test.database.other-users.grafana.enabled}}
+-- 
+-- SQL script ends here
