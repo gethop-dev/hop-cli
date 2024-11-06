ee364627965f4b8290de2fa7d1d95540c7a5de09
Iñaki Arenaza <inaki.arenaza@biotz.io>
2024-11-06 10:01:46 +0100

    Upgrade base dependencies for the generated project.

--- resources/bootstrap/profiles/core/app/project.clj
+++ resources/bootstrap/profiles/core/app/project.clj
@@ -1,18 +1,19 @@
 (defproject {{project.name}} "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :min-lein-version "2.9.8"
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [duct/core "0.8.0"]
-                 [ring "1.9.6"]
+  :min-lein-version "2.12.2"
+  :dependencies [[org.clojure/clojure "1.11.4"]
+                 [duct/core "0.8.1"]
+                 [ring/ring "1.13.0"]
                  [duct/server.http.http-kit "0.1.4"
                   :exclusions [[http-kit]]]
-                 [http-kit "2.8.0"]
+                 [http-kit/http-kit "2.8.0"]
                  [duct/module.logging "0.5.0"]
                  [duct/logger.timbre "0.5.0"]
-                 [metosin/jsonista "0.3.8"]
-                 [metosin/reitit "0.6.0"]
-                 [metosin/malli "0.13.0"]
+                 [com.taoensso/timbre "6.6.1"]
+                 [fi.metosin/jsonista "0.3.12"]
+                 [fi.metosin/reitit "0.7.2"]
+                 [fi.metosin/malli "0.16.4"]
                  [com.widdindustries/cljc.java-time "0.1.21"]
                  {{#project.dependencies}}{{&.}}{{/project.dependencies}}]
   :plugins [[duct/lein-duct "0.12.3"]]
@@ -21,27 +22,26 @@
   :middleware [lein-duct.plugin/middleware]
   :profiles {:dev [:project/dev :profiles/dev]
              :repl {:prep-tasks ^:replace ["javac" "compile"]
-                    :dependencies [[cider/piggieback "0.5.2"]]
+                    :dependencies [[cider/piggieback "0.5.3"]]
                     :repl-options {:init-ns user
                                    :nrepl-middleware [cider.piggieback/wrap-cljs-repl]
                                    :host "0.0.0.0"
                                    :port 4001}}
              :uberjar {:aot :all :prep-tasks ["javac" "compile" ["run" ":duct/compiler"]]}
              :profiles/dev {}
-             :project/dev
-             {:eastwood {:linters [:all]
-                         :exclude-linters
-                         [:keyword-typos :boxed-math :non-clojure-file :performance :unused-namespaces]
-                         :debug [:progress :time]}
-              :resource-paths ["dev/resources"]
-              :source-paths ["dev/src"]
-              :plugins [[dev.weavejester/lein-cljfmt "0.11.2"]
-                        [jonase/eastwood "1.4.0"]]
-              :dependencies [[integrant/repl "0.3.2"]
-                             [hawk "0.2.11"]
-                             [eftest "0.5.9"]
-                             [ring/ring-mock "0.4.0"]
-                             {{#project.dev-dependencies}}{{&.}}{{/project.dev-dependencies}}]}}
+             :project/dev {:eastwood {:linters [:all]
+                                      :exclude-linters
+                                      [:keyword-typos :boxed-math :non-clojure-file :performance :unused-namespaces]
+                                      :debug [:progress :time]}
+                           :resource-paths ["dev/resources"]
+                           :source-paths ["dev/src"]
+                           :plugins [[dev.weavejester/lein-cljfmt "0.13.0"]
+                                     [jonase/eastwood "1.4.3"]]
+                           :dependencies [[integrant/repl "0.4.0"]
+                                          [hawk/hawk "0.2.11"]
+                                          [eftest/eftest "0.6.0"]
+                                          [ring/ring-mock "0.4.0"]
+                                          {{#project.dev-dependencies}}{{&.}}{{/project.dev-dependencies}}]}}
   :uberjar-name "{{project.name}}-standalone.jar"
   :test-selectors {:default (fn [m] (not (or (:integration m) (:regression m))))
                    :all (constantly true)
