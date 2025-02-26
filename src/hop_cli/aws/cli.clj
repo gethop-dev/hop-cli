gethop-dev/hop-cli

commit a314a3a55d941abbd7bc56c0ff62d2548144c1ec

Refactor error handling and include --help support everywhere

src/hop_cli/aws/cli.clj
@@ -13,6 +13,9 @@
             [hop-cli.util.error :as error]
             [hop-cli.util.help :as help]))
 
+(def ^:const main-cmd
+  "aws")
+
 (defn- generic-handler-wrapper
   [handler-fn {:keys [opts]}]
   (pprint (handler-fn opts)))
@@ -24,138 +27,146 @@
 
 (declare print-help-handler)
 
+(def ^:const region-spec
+  [:region
+   {:alias :r
+    :desc "AWS Region where the resource is defined."
+    :default-desc "Optional (default empty)"}])
+
+(def ^:const project-name-spec
+  [:project-name
+   {:alias :p
+    :require true
+    :desc "Name of the project for the action."}])
+
+(def ^:const environment-spec
+  [:environment
+   {:alias :e
+    :desc "Target environment for the action (get, sync, etc.). E.g., prod, test."
+    :require true}])
+
+(def kms-key-alias-spec
+  [:kms-key-alias
+   {:alias :k :require true
+    :desc "Alias of the AWS KMS Key that will be used to encrypt or decrypt the environment variables."}])
+(def ^:const file-spec
+  [:file
+   {:alias :f
+    :require true
+    :desc "Path where the environment variables will be saved to or loaded from."}])
+
+(def ^:const user-pool-id-spec
+  [:user-pool-id
+   {:alias :up
+    :require true
+    :desc "ID of the AWS Cognito User Pool where the action will be performed."}])
+
+(def ^:const username-spec
+  [:username
+   {:alias :u
+    :require true
+    :desc "Username or email to use for the action."}])
+
+(def ^:const password-spec
+  [:password
+   {:alias :p
+    :require true
+    :desc ""}])
+
 (defn- cli-cmd-table
   []
   [;; Environment Variable manager
    {:cmds ["env-vars" "sync"]
     :fn (partial generic-handler-wrapper env-vars/sync-env-vars)
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "env-vars" "sync"])
     :desc "Command for synchronizing local environment variables with the remote environment variables storage in AWS SSM Parameter Store."
-    :spec {:project-name
-           {:alias :p :require true}
-           :environment
-           {:alias :e
-            :desc "Target environment to update. E.g., prod, test"
-            :require true}
-           :file
-           {:alias :f :require true}
-           :kms-key-alias
-           {:alias :k :require true
-            :desc "Alias of the AWS KMS Key that will be used to encrypt the environment variables."}
-           :region
-           {:alias :r :require false
-            :desc "Region"}}}
-
+    :spec (help/with-help-spec
+            [project-name-spec
+             environment-spec
+             file-spec
+             kms-key-alias-spec
+             region-spec])}
    {:cmds ["env-vars" "download"]
     :fn (partial generic-handler-wrapper env-vars/download-env-vars)
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "env-vars" "download"])
     :desc "Command for downloading the environment variables from AWS SSM Parameter Store into a file."
-    :spec {:project-name
-           {:alias :p :require true}
-           :environment
-           {:alias :e
-            :desc "Environment from where the variables will be obtained. E.g., prod, test"
-            :require true}
-           :file
-           {:alias :f
-            :desc "Path where the environment variables will be saved to."
-            :require true}
-           :kms-key-alias
-           {:alias :k :require true
-            :desc "Alias of the AWS KMS Key that will be used to decrypt the environment variables."}
-           :region
-           {:alias :r :require false
-            :desc "Region"}}}
-
+    :spec (help/with-help-spec
+            [project-name-spec
+             environment-spec
+             file-spec
+             kms-key-alias-spec
+             region-spec])}
    {:cmds ["env-vars" "apply-changes"]
     :fn (partial generic-handler-wrapper env-vars/apply-env-var-changes)
-    :error-fn error/generic-error-handler
-    :desc "Command for triggering an AWS Elastic Beanstalk environment restart. In order to update the environment variables in AWS Elastic Beanstalk, the environment has to be restarted. This can be done automatically by AWS (deploying a new application version...), using the AWS Console, or by running this command."
-    :spec {:project-name
-           {:alias :p :require true}
-           :environment
-           {:alias :e :require true}
-           :region
-           {:alias :r :require false
-            :desc "Region"}}}
+    :error-fn (partial error/generic-error-handler [main-cmd "env-vars" "apply-changes"])    :desc "Command for triggering an AWS Elastic Beanstalk environment restart. In order to update the environment variables in AWS Elastic Beanstalk, the environment has to be restarted. This can be done automatically by AWS (deploying a new application version...), using the AWS Console, or by running this command."
+    :spec (help/with-help-spec
+            [project-name-spec
+             environment-spec
+             region-spec])}
 
    ;; SSL manager
    {:cmds ["ssl" "create-and-upload-self-signed-certificate"]
     :fn (partial generic-handler-wrapper ssl/create-and-upload-self-signed-certificate)
-    :error-fn error/generic-error-handler
-    :desc "Command for creating and uploading a self-signed certificate to AWS Certificate Manager."
-    :spec {:region
-           {:alias :r :require false
-            :desc "Region"}}}
+    :error-fn (partial error/generic-error-handler [main-cmd "ssl" "create-and-upload-self-signed-certificate"])    :desc "Command for creating and uploading a self-signed certificate to AWS Certificate Manager."
+    :spec (help/with-help-spec
+            [region-spec])}
 
    ;; Cognito
    {:cmds ["cognito" "create-user"]
     :fn cognito-create-user-handler
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "cognito" "create-user"])
     :desc "Create user in the specified AWS Cognito User Pool."
-    :spec {:user-pool-id
-           {:alias :up :require true}
-           :username
-           {:alias :u :require true
-            :desc "Username or email"}
-           :attributes
-           {:alias :a
-            :coerce []
-            :desc "User attributes in the form of `ATTRIBUTE1=VAL1 ATTRIBUTE2=VAL2`"}
-           :temporary-password
-           {:alias :p}
-           :region
-           {:alias :r :require false
-            :desc "Region"}}}
-
+    :spec (help/with-help-spec
+            [user-pool-id-spec
+             username-spec
+             [:temporary-password
+              {:alias :p
+               :desc "Temporary password assigned to the user. It will have to be changed on first login."}]
+             [:attributes
+              {:alias :a
+               :coerce []
+               :desc "User attributes in the form of 'ATTRIBUTE1=VAL1 ATTRIBUTE2=VAL2'"}]
+             region-spec])}
    {:cmds ["cognito" "set-user-password"]
     :fn (partial generic-handler-wrapper cognito/admin-set-user-password)
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "cognito" "set-user-password"])
     :desc "Change the password of the specified user."
-    :spec {:user-pool-id
-           {:alias :up :require true}
-           :username
-           {:alias :u :require true
-            :desc "Username or email"}
-           :password
-           {:alias :p :require true}
-           :temporary?
-           {:alias :t}
-           :region
-           {:alias :r :require false
-            :desc "Region"}}}
-
+    :spec (help/with-help-spec
+            [user-pool-id-spec
+             username-spec
+             (update password-spec 1 (fn [m] (assoc m :desc "New password to be assigned to the user.")))
+             [:temporary?
+              {:alias :t
+               :coerce boolean
+               :desc "Whether the new password is temporary. If so, it will have to be changed on first login."}]
+             region-spec])}
    {:cmds ["cognito" "get-id-token"]
     :fn (partial generic-handler-wrapper cognito/get-id-token)
-    :error-fn error/generic-error-handler
-    :desc "Get OIDC identity token for the specified user."
-    :spec {:user-pool-id
-           {:alias :up :require true}
-           :client-id
-           {:alias :c :require true}
-           :username
-           {:alias :u :require true
-            :desc "Username or email"}
-           :password
-           {:alias :p :require true}
-           :region
-           {:alias :r :require false
-            :desc "Region"}}}
+    :error-fn (partial error/generic-error-handler [main-cmd "cognito" "get-id-token"])
+    :desc "Get an OpenID Connect Identity token for the specified user."
+    :spec (help/with-help-spec
+            [user-pool-id-spec
+             [:client-id
+              {:alias :c
+               :require true
+               :desc "Client ID to use to get the Identity token."}]
+             username-spec
+             (update password-spec 1 (fn [m] (assoc m :desc "Password of the AWS Cognito account.")))
+             region-spec])}
+
    ;; RDS
    {:cmds ["rds" "start-port-forwarding-session"]
     :fn (partial generic-handler-wrapper rds/start-port-forwarding-session)
-    :error-fn error/generic-error-handler
-    :desc "Execute command to start a port forwarding session to a RDS instance. \nIn order to use this command you will need to have installed `awscli` and the `AWS Session Manager plugin` for the mentioned tool."
-    :spec {:project-name
-           {:alias :p :require true}
-           :environment
-           {:alias :e :require true}
-           :local-port
-           {:alias :lp :require true
-            :desc "Local port"}
-           :region
-           {:alias :r :require false
-            :desc "Region"}}}
+    :error-fn (partial error/generic-error-handler [main-cmd "rds" "start-port-forwarding-session"])
+    :desc "Execute command to start a port forwarding session to a RDS instance. \nIn order to use this command you will need to have the AWS CLI installed, and the `AWS Session Manager plugin` for the mentioned tool."
+    :spec (help/with-help-spec
+            [project-name-spec
+             environment-spec
+             [:local-port
+              {:alias :lp
+               :require true
+               :desc "Local port forwarded to the remote RDS instance."}]
+             region-spec])}
 
    ;; Help
    {:cmds []
@@ -163,7 +174,7 @@
 
 (defn- print-help-handler
   [_]
-  (help/print-help (cli-cmd-table) "aws"))
+  (help/print-help (cli-cmd-table) main-cmd))
 
 (defn main [args]
   (cli/dispatch (cli-cmd-table) args))

src/hop_cli/keycloak/cli.clj
@@ -11,6 +11,9 @@
             [hop-cli.util.error :as error]
             [hop-cli.util.help :as help]))
 
+(def ^:const main-cmd
+  "keycloak")
+
 (defn with-access-token
   [opts call-fn]
   (let [result (api.openid-connect/get-admin-access-token opts)]
@@ -22,17 +25,38 @@
 
 (declare print-help-handler)
 
+(def ^:const base-url-spec
+  [:base-url
+   {:alias :bu
+    :require true
+    :desc "Base URL for the Keycloak instance."}])
+
 (def ^:const admin-auth-spec
-  [[:base-url
-    {:alias :bu :require true}]
+  [base-url-spec
    [:admin-realm-name
-    {:alias :ar :default "master"}]
+    {:alias :ar
+     :default "master"
+     :default-desc "(Optional, default value: master)"
+     :desc "Name of the Keycloak Realm where the 'admin-user' exists."}]
    [:admin-client-id
-    {:alias :ac :default "admin-cli"}]
+    {:alias :ac
+     :default "admin-cli"
+     :default-desc "(Optional, default value: admin-cli)"
+     :desc "Client Id to use with the 'admin-realm-name'."}]
    [:admin-username
-    {:alias :au :require true}]
+    {:alias :au
+     :require true
+     :desc "Username with administrator rights to perform the action."}]
    [:admin-password
-    {:alias :ap :require true}]])
+    {:alias :ap
+     :require true
+     :desc "Password for the `admin-username`."}]])
+
+(def ^:const realm-name-spec
+  [:realm-name
+   {:alias :r
+    :require true
+    :desc "Keycloak Realm to use for the action."}])
 
 (defn- cli-cmd-table
   []
@@ -41,72 +65,97 @@
              (update :attributes util/cli-stdin-map->map)
              (with-access-token api.user/create-user)
              (pprint))
-    :error-fn error/generic-error-handler
-    :desc "Create user in the specified Keycloak Realm."
-    :spec (concat
-           admin-auth-spec
-           [[:realm-name
-             {:alias :r :require true}]
-            [:username
-             {:alias :u :require true}]
-            [:temporary-password
-             {:alias :p}]
-            [:attributes
-             {:alias :a
-              :coerce []
-              :desc "User attributes in the form of `ATTRIBUTE1=VAL1 ATTRIBUTE2=VAL2`"}]
-            [:first-name {}]
-            [:last-name {}]
-            [:email {}]
-            [:email-verified {}]])}
+    :error-fn (partial error/generic-error-handler [main-cmd "create-user"])
+    :desc "Create a user in the specified Keycloak Realm."
+    :spec (help/with-help-spec
+            (into admin-auth-spec
+                  [realm-name-spec
+                   [:username
+                    {:alias :u
+                     :require true
+                     :desc "Username for the new user to be created."}]
+                   [:temporary-password
+                    {:alias :p
+                     :require true
+                     :desc "Temporary password assigned to the user. It will have to be changed on first login."}]
+                   [:attributes
+                    {:alias :a
+                     :coerce []
+                     :desc "User attributes in the form of 'ATTRIBUTE1=VAL1 ATTRIBUTE2=VAL2'"
+                     :default-desc "(Optional, default empty)"}]
+                   [:first-name
+                    {:desc "First name for the new user to be created."
+                     :default-desc "(Optional, default empty)"}]
+                   [:last-name
+                    {:desc "Last name for the new user to be created."
+                     :default-desc "(Optional, default empty)"}]
+                   [:email
+                    {:desc "Email address for the new user to be created."
+                     :default-desc "(Optional, default empty)"}]
+                   [:email-verified
+                    {:desc "Set the new use email address as verified."
+                     :coerce boolean
+                     :default false
+                     :default-desc "(Optional, default false)"}]]))}
    {:cmds ["set-user-password"]
     :fn #(-> (:opts %1)
              (with-access-token api.user/set-user-password)
              (pprint))
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "set-user-password"])
     :desc "Change the password of the specified user."
-    :spec (concat
-           admin-auth-spec
-           [[:realm-name
-             {:alias :r :require true}]
-            [:user-id
-             {:alias :u :require true}]
-            [:password
-             {:alias :p :require true}]
-            [:temporary?
-             {:alias :t}]])}
+    :spec (help/with-help-spec
+            (into admin-auth-spec
+                  [realm-name-spec
+                   [:user-id
+                    {:alias :u
+                     :require true
+                     :desc "Keycloak internal ID for the user (you can get it with the 'get-user' command)."}]
+                   [:password
+                    {:alias :p
+                     :require true
+                     :desc "New password to be assigned to the user"}]
+                   [:temporary?
+                    {:alias :t
+                     :coerce boolean
+                     :desc "Whether the new password is temporary. If so, it will have to be changed on first login."}]]))}
    {:cmds ["get-user"]
     :fn #(-> (:opts %1)
              (with-access-token api.user/get-user)
              (pprint))
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "get-user"])
     :desc "Get the details about the specified Keycloak user."
-    :spec (concat
-           admin-auth-spec
-           [[:realm-name
-             {:alias :r :require true}]
-            [:username
-             {:alias :u :require true}]])}
+    :spec (help/with-help-spec
+            (into admin-auth-spec
+                  [realm-name-spec
+                   [:username
+                    {:alias :u
+                     :require true
+                     :desc "What username to perform the operation for."}]]))}
    {:cmds ["get-id-token"]
     :fn #(pprint (api.openid-connect/get-id-token (:opts %1)))
-    :error-fn error/generic-error-handler
-    :desc "Get OIDC identity token for the specified user."
-    :spec [[:base-url
-            {:alias :bu :require true}]
-           [:realm-name
-            {:alias :r :require true}]
-           [:client-id
-            {:alias :c :require true}]
-           [:username
-            {:alias :u :require true}]
-           [:password
-            {:alias :p :require true}]]}
+    :error-fn (partial error/generic-error-handler [main-cmd "get-id-token"])
+    :desc "Get an OpenID Connect Identity token for the specified user."
+    :spec (help/with-help-spec
+            [base-url-spec
+             realm-name-spec
+             [:client-id
+              {:alias :c
+               :require true
+               :desc "Client ID to use to get the Identity token."}]
+             [:username
+              {:alias :u
+               :require true
+               :desc "What username to get the token for."}]
+             [:password
+              {:alias :p
+               :require true
+               :desc "The username's password."}]])}
    {:cmds []
     :fn print-help-handler}])
 
 (defn- print-help-handler
   [_]
-  (help/print-help (cli-cmd-table) "keycloak"))
+  (help/print-help (cli-cmd-table) main-cmd))
 
 (defn main [args]
   (cli/dispatch (cli-cmd-table) args))

src/hop_cli/bootstrap/cli.clj
@@ -12,6 +12,9 @@
             [hop-cli.util.error :as error]
             [hop-cli.util.help :as help]))
 
+(def ^:const main-cmd
+  "bootstrap")
+
 (defn- bootstrap-handler
   [environments {:keys [opts]}]
   (pprint (main/bootstrap-hop (assoc opts :environments environments))))
@@ -30,63 +33,69 @@
   []
   [{:cmds ["new-project"]
     :fn (partial bootstrap-handler [:dev :test])
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "new-project"])
     :desc "Command for bootstrapping a new HOP Application. That includes provisioning the infrastructure and generating the project files."
-    :spec {:settings-file-path {:alias :s
-                                :desc "Path to the HOP CLI settings file."
-                                :require true
-                                :validate (comp fs/exists? fs/file)
-                                :error-msgs {:validate "Couldn't find HOP settings file. Aborting..."}}
-           :target-project-dir {:alias :d
-                                :desc "Directory in which the new project will be created."
-                                :require true
-                                :validate (comp not fs/exists? fs/file)
-                                :error-msgs {:validate "Project directory already exists. Please input a different directory."}}}}
+    :spec (help/with-help-spec
+            [[:settings-file-path {:alias :s
+                                   :desc "Path to the HOP CLI settings file."
+                                   :require true
+                                   :validate (comp fs/exists? fs/file)
+                                   :error-msgs {:validate "Couldn't find HOP settings file. Aborting..."}}]
+             [:target-project-dir {:alias :d
+                                   :desc "Directory in which the new project will be created."
+                                   :require true
+                                   :validate (comp not fs/exists? fs/file)
+                                   :error-msgs {:validate "Project directory already exists. Please input a different directory."}}]])}
    {:cmds ["create-settings-file"]
     :fn copy-settings-handler
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "create-settings-handler"])
     :desc "Creates a file with a copy of the default HOP CLI configuration."
-    :spec {:settings-file-path {:alias :s
-                                :desc "Path where the settings file will be copied to."
-                                :require true}}}
+    :spec (help/with-help-spec
+            [[:settings-file-path {:alias :s
+                                   :desc "Path where the settings file will be copied to."
+                                   :require true}]])}
    {:cmds ["copy-settings"]
     :fn copy-settings-handler
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "copy-settings"])
     :desc "An alias for create-settings-file."
-    :spec {:settings-file-path {:alias :s
-                                :desc "Path where the settings file will be copied to."
-                                :require true}}}
+    :spec (help/with-help-spec
+            [[:settings-file-path {:alias :s
+                                   :desc "Path where the settings file will be copied to."
+                                   :require true}]])}
    {:cmds ["prod-infrastructure"]
     :fn (partial bootstrap-handler [:prod])
-    :error-fn error/generic-error-handler
+    :error-fn (partial error/generic-error-handler [main-cmd "prod-infrastructure"])
     :desc "Command for provisioning the infrastructure for the production environment."
-    :spec {:settings-file-path {:alias :s
-                                :desc "Path to the HOP CLI settings file. It should be the same file used when bootstrapping the HOP Application."
-                                :require true
-                                :validate (comp fs/exists? fs/file)
-                                :error-msgs {:validate "Couldn't find HOP setting.edn file. Aborting..."}}
-           :target-project-dir {:alias :d
-                                :desc "Directory where the project is located."
-                                :require true
-                                :validate (comp fs/exists? fs/file)
-                                :error-msgs {:validate "Project directory must exist. Please input a different directory."}}}}
+    :spec (help/with-help-spec
+            [[:settings-file-path {:alias :s
+                                   :desc "Path to the HOP CLI settings file. It should be the same file used when bootstrapping the HOP Application."
+                                   :require true
+                                   :validate (comp fs/exists? fs/file)
+                                   :error-msgs {:validate "Couldn't find HOP setting.edn file. Aborting..."}}]
+             [:target-project-dir {:alias :d
+                                   :desc "Directory where the project is located."
+                                   :require true
+                                   :validate (comp fs/exists? fs/file)
+                                   :error-msgs {:validate "Project directory must exist. Please input a different directory."}}]])}
    {:cmds ["open-settings-editor"]
     :fn open-settings-editor-handler
-    :error-fn error/generic-error-handler
-    :spec {:port {:alias :p
-                  :default 8090
-                  :desc "Port on which the web server will be launched."
-                  :validate pos-int?
-                  :coerce :int
-                  :error-msgs {:validate "Invalid port number format."
-                               :coerce "Port is not a valid number. Aborting... "}}}
-    :desc "Opens a web-based wizard for creating or editing the settings file."}
+    :error-fn (partial error/generic-error-handler [main-cmd "open-settings-editor"])
+    :desc "Opens a web-based wizard for creating or editing the settings file."
+    :spec (help/with-help-spec
+            [[:port {:alias :p
+                     :default 8090
+                     :desc "Port on which the web server will be launched."
+                     :default-desc "(Optional, default: 8090)"
+                     :validate pos-int?
+                     :coerce :int
+                     :error-msgs {:validate "Invalid port number format."
+                                  :coerce "Port is not a valid number. Aborting... "}}]])}
    {:cmds []
     :fn print-help-handler}])
 
 (defn- print-help-handler
   [_]
-  (help/print-help (cli-cmd-table) "bootstrap"))
+  (help/print-help (cli-cmd-table) main-cmd))
 
 (defn main
   [args]

src/hop_cli/util/error.clj
@@ -3,15 +3,18 @@
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 (ns hop-cli.util.error
-  (:require [babashka.cli :as cli]))
+  (:require [hop-cli.util.help :as help]))
 
 (defn get-error-msg
   [{:keys [spec cause msg option]}]
   (or (get-in spec [option :error-msgs cause]) msg))
 
 (defn generic-error-handler
-  [{:keys [spec] :as error}]
-  (println "An error has occurred:" (get-error-msg error))
-  (println "Usage:")
-  (println (cli/format-opts {:spec spec}))
-  (System/exit 1))
+  [cmd-path cmd]
+  (when-not (-> cmd :opts :help)
+    (println "An error has occurred:" (get-error-msg cmd))
+    (println))
+  (help/print-cmd-help cmd-path cmd)
+  (if (-> cmd :opts :help)
+    (System/exit 0)
+    (System/exit 1)))

src/hop_cli/util/help.clj
@@ -3,7 +3,8 @@
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/
 
 (ns hop-cli.util.help
-  (:require [clojure.pprint :as pprint]
+  (:require [babashka.cli :as cli]
+            [clojure.pprint :as pprint]
             [clojure.string :as str]
             [hop-cli.util :as util]))
 
@@ -22,6 +23,21 @@
       (clojure.string/split #" ")
       (->> (pprint/cl-format nil (str "~{~<~%~1," wrap-size ":;~A~> ~}")))
       (str/split #"\n")))
+
+(defn with-help-spec
+  [specs]
+  (conj specs
+        [:help {:desc "Show this help."
+                ;; Hack to handle `--help` in the error fn.
+                :validate (constantly false)}]))
+
+(defn print-cmd-help
+  [cmd-path cmd]
+  (println (format "Version: %s" (util/get-version)))
+  (println (pprint/cl-format nil "Usage:~{ ~A~} <options>" cmd-path))
+  (println)
+  (println "Options")
+  (println (cli/format-opts cmd)))
 
 (defn print-help
   ([cmds]

commit 34526d1f8e47ef999fce682e744a4786832b0f4f

Fix --temporary? option handling
Commit a314a3a55d9 broke the handling of this value. We (wrongly)
assumed that the coercing function would get a value that would either
be true (if specified), false (also, if specified), or nil (if not
specified). But in the two former cases we get "true" and "false", as
strings. Thus, using the standard `boolean` function always returned a
truthy value (they are non-nil strings), no matter what was specified
in the command line.

Using the Babashka CLI provided `:boolean` coercer does the right
thing in those cases.

src/hop_cli/aws/cli.clj
@@ -137,7 +137,7 @@
              (update password-spec 1 (fn [m] (assoc m :desc "New password to be assigned to the user.")))
              [:temporary?
               {:alias :t
-               :coerce boolean
+               :coerce :boolean
                :desc "Whether the new password is temporary. If so, it will have to be changed on first login."}]
              region-spec])}
    {:cmds ["cognito" "get-id-token"]

src/hop_cli/keycloak/cli.clj
@@ -119,7 +119,7 @@
                 :default-desc "(Optional, default empty)"}]
               [:email-verified
                {:desc "Set the new use email address as verified."
-                :coerce boolean
+                :coerce :boolean
                 :default false
                 :default-desc "(Optional, default false)"}]]))}
    {:cmds ["set-user-password"]
@@ -142,7 +142,7 @@
                 :desc "New password to be assigned to the user"}]
               [:temporary?
                {:alias :t
-                :coerce boolean
+                :coerce :boolean
                 :desc "Whether the new password is temporary. If so, it will have to be changed on first login."}]]))}
    {:cmds ["get-user"]
     :fn #(-> (:opts %1)
@@ -189,7 +189,7 @@
               [:raw
                {:require false
                 :default false
-                :coerce boolean
+                :coerce :boolean
                 :desc "Output id-token in raw format."
                 :default-desc "(Optional)"}]]))}
    {:cmds []