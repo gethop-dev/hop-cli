8843314a67fcefc434bcbf0412683326176ad049
Iñaki Arenaza <inaki.arenaza@biotz.io>
2025-08-14 17:23:47 +0200

    [cosmetic] Add missing separation line

--- resources/bootstrap/profiles/core/app/dev/src/dev.clj
+++ resources/bootstrap/profiles/core/app/dev/src/dev.clj
@@ -14,7 +14,6 @@
             [clojure.tools.namespace.repl :refer [refresh]]
             [duct.core :as duct]
             [duct.core.repl :as duct-repl :refer [auto-reset]]
-            [eftest.runner :as eftest]
             [fipp.edn :refer [pprint]]
             [integrant.core :as ig]
             [integrant.repl :refer [clear halt go init prep reset]]
@@ -24,9 +23,6 @@
 
 (defn read-config []
   (duct/read-config (io/resource "<<project.files-name>>/config.edn")))
-
-(defn test []
-  (eftest/run-tests (eftest/find-tests "test")))
 
 (def profiles
   [:duct.profile/dev :duct.profile/local])
